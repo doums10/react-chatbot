fd30ab5c1b287ae43d14e8aaef94c8591a2c62d0
mamadou sylla <m27sylla@gmail.com>
2019-12-23 13:52:26 +0100

    Updates

---
+++ precache-manifest.c41d6e7ba42a8f32e79ae945d3041f61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2225e0859016e78bd174e7398f14c073",
+    "url": "/react-chatbot/index.html"
+  },
+  {
+    "revision": "bf60d47d15772922fb97",
+    "url": "/react-chatbot/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "88da83d3c190061590db",
+    "url": "/react-chatbot/static/css/main.8fdb8fac.chunk.css"
+  },
+  {
+    "revision": "bf60d47d15772922fb97",
+    "url": "/react-chatbot/static/js/2.4415c6b5.chunk.js"
+  },
+  {
+    "revision": "5ef03fc7163c9947bff1fb458d073d8e",
+    "url": "/react-chatbot/static/js/2.4415c6b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88da83d3c190061590db",
+    "url": "/react-chatbot/static/js/main.b2f634fe.chunk.js"
+  },
+  {
+    "revision": "5184377f1470b2abcf13",
+    "url": "/react-chatbot/static/js/runtime-main.9975130d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-chatbot/static/media/logo.5d5d9eef.svg"
+  }
+]);
